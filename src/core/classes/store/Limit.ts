f03e4a0803fe8f968b63707ab080db6216a26d8b
aafxr <alexandral.fed23@gmail.com>
2024-04-11 11:24:31 +0700

    limit class

---
+++ src/core/classes/store/Limit.ts
@@ -0,0 +1,36 @@
+import {DBFlagType} from "../../../types/DBFlagType";
+
+
+/**
+ * Класс для работы с лимитами
+ *
+ * id лимита формируется как:
+ * - "section_id:primary_entity_id" для общих лимитов
+ * - "user_id:section_id:primary_entity_id" для общих личных
+ *
+ * Содержи поля:
+ *
+ * __id__,
+ * __personal__,
+ * __section_id__,
+ * __value__,
+ * __primary_entity_id__
+ */
+export class Limit {
+
+    id: string
+    personal: DBFlagType
+    section_id: string
+    value: number
+    primary_entity_id: string;
+
+    constructor(limit?: Partial<Limit>) {
+        if(!limit) limit = {}
+
+        this.id                 = limit.id !== undefined ? limit.id : ''
+        this.personal           = limit.personal !== undefined ? limit.personal : 0
+        this.section_id         = limit.section_id !== undefined ? limit.section_id : ''
+        this.value              = limit.value !== undefined ? limit.value : 0
+        this.primary_entity_id  = limit.primary_entity_id !== undefined ? limit.primary_entity_id : ''
+    }
+}
