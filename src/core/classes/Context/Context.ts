975cc03ea743edab3e0d7db93ab7408ecf3f31a2
aafxr <alexandral.fed23@gmail.com>
2024-04-12 10:00:29 +0700

    app context dependency

---
+++ src/core/classes/Context/Context.ts
@@ -0,0 +1,39 @@
+import EventEmitter from "../EventEmmiter";
+import {Socket} from "socket.io-client";
+import {Travel, User} from "../store";
+
+export class Context extends EventEmitter {
+    user: User | null = null
+    travel: Travel | null = null
+    socket: Socket | null = null
+
+    constructor(context?: Context) {
+        super();
+        if(!context) return
+
+        if(context.user) this.user = context.user
+        if(context.travel) this.travel = context.travel
+        if(context.socket) this.socket = context.socket
+    }
+
+
+    setUser(user: User | null) {
+        this.user = user
+        this.emit('update', [this])
+    }
+
+    setTravel(travel: Travel | null) {
+        this.travel = travel
+        this.emit('update', [this])
+    }
+
+    setSocket(socket: Socket | null){
+        this.socket = socket
+        this.emit('update', [this])
+    }
+
+    get isLogIn(){
+        return !!(this.user && Boolean(this.user.token) && Boolean(this.user.refresh_token));
+    }
+
+}
