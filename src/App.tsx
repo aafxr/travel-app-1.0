2f7ab1d73639acc185358f47c380271d9e8f05ca
aafxr <alexandral.fed23@gmail.com>
2024-04-15 15:29:13 +0700

    default app files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {SubjectContextProvider} from "./contexts/SubjectContextProvider";
+
+function App() {
+
+
+    return (
+        <SubjectContextProvider>
+
+        </SubjectContextProvider>
+    );
+}
+
+export default App;
