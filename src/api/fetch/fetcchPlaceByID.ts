e227fa69e8490385a8f1cfa823ed9a701e85547d
aafxr <alexandral.fed23@gmail.com>
2024-04-15 10:38:16 +0700

    fetcchPlaceByID response type

--- src/api/fetch/fetcchPlaceByID.ts
+++ src/api/fetch/fetcchPlaceByID.ts
@@ -1,6 +1,14 @@
 import aFetch from "../../axios";
+import {Place} from "../../core/classes/store/Place";
+
+type FetchPlaceByIDResponseType = {
+    data: Place
+    ok: boolean
+    request: Record<string, any>
+}
+
 
 export async function fetchPlaceByID(placeID: string){
-    return (await aFetch.post<any>('/places/getByID/', {id: placeID})).data
+    return (await aFetch.post<FetchPlaceByIDResponseType>('/places/getByID/', {id: placeID})).data
 
 }
