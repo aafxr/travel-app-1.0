3748a80fd2fb2e90eddbaf1ba2bd242fc8aeecfd
aafxr <alexandral.fed23@gmail.com>
2024-04-18 16:47:38 +0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import {useEffect, useState} from "react";
 import {Navigate, Route, Routes} from "react-router-dom";
-import {Events, Favorite, Login, Main, Profile, TravelAdd, TravelRoutes, TravelSettings} from "./pages";
+
+import {Dev, Events, Favorite, Login, Main, Profile, TravelAdd, TravelRoutes, TravelSettings} from "./pages";
+import defaultHandleError from "./utils/error-handlers/defaultHandleError";
+import {useAppContext} from "./contexts/AppContextProvider";
+import {UserController} from "./core/service-controllers";
 import AuthRequired from "./hoc/AuthRequired";
+import Container from "./components/Container/Container";
+import Loader from "./components/Loader/Loader";
+import {RouteAdvice} from "./pages/RouteAdvice";
 
 
 function App() {
+    const context = useAppContext()
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        if (!context.user)
+            setLoading(true)
+        UserController.getLoggedInUser(context)
+            .then((user) => {
+                if (user) context.setUser(user)
+            })
+            .catch(defaultHandleError)
+            .finally(() =>  setLoading(false) )
+    }, [context])
+
+    if(loading)
+        return (
+            <Container className='center h-full'>
+                <Loader/>
+            </Container>
+        )
 
     return (
         <Routes>
@@ -16,7 +44,9 @@ function App() {
                 <Route path={'/travels/:travelType/'} element={<TravelRoutes/>}/>
                 <Route path={'/travel/add/'} element={<TravelAdd/>}/>
                 <Route path={'/travel/:travelCode/settings/'} element={<TravelSettings/>}/>
+                <Route path={'/travel/:travelCode/advice-route/'} element={<RouteAdvice/>}/>
             </Route>
+            <Route path={'/dev/'} element={<Dev/>}/>
             <Route path={'*'} element={<Navigate to={'/'} replace/>}/>
         </Routes>
     );
