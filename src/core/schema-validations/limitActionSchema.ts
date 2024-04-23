42ecfc3a443c317feaaa9633d06d7e6a1869ed42
aafxr <alexandral.fed23@gmail.com>
2024-04-23 09:25:39 +0700

    validation schema change

--- src/core/schema-validations/limitActionSchema.ts
+++ src/core/schema-validations/limitActionSchema.ts
@@ -2,15 +2,15 @@ import Joi from "joi";
 import {StoreName} from "../../types/StoreName";
 
 export const limitActionSchema = Joi.object({
-    id: Joi.string().required(),
-    uid: Joi.string().required(),
+    id: Joi.string().required().min(7),
+    uid: Joi.string().required().min(7),
     action: Joi.string().required(),
-    user_id: Joi.string().required(),
+    user_id: Joi.string().required().min(7),
     synced: Joi.number().required(),
     entity: Joi.string().equal(StoreName.LIMIT).required(),
     datetime: Joi.number().required(),
     data: Joi.object({
-        id: Joi.string().required(),
-        primary_entity_id: Joi.string().required()
+        id: Joi.string().required().min(7),
+        primary_entity_id: Joi.string().required().min(7)
     }).options({allowUnknown: true})
 })
--- src/core/schema-validations/photoActionSchema.ts
+++ src/core/schema-validations/photoActionSchema.ts
@@ -2,15 +2,15 @@ import Joi from "joi";
 import {StoreName} from "../../types/StoreName";
 
 export const photoActionSchema = Joi.object({
-    id: Joi.string().required(),
-    uid: Joi.string().required(),
+    id: Joi.string().required().min(7),
+    uid: Joi.string().required().min(7),
     action: Joi.string().required(),
-    user_id: Joi.string().required(),
+    user_id: Joi.string().required().min(7),
     synced: Joi.number().required(),
     entity: Joi.string().equal(StoreName.Photo).required(),
     datetime: Joi.number().required(),
     data: Joi.object({
         id: Joi.string().required(),
-        base64: Joi.string().required()
+        base64: Joi.string().required().base64()
     }).options({allowUnknown: true})
 })
