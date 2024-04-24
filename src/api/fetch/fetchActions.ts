455710181cfa9edb976764975c4bb85e52b71131
aafxr <alexandral.fed23@gmail.com>
2024-04-24 10:59:27 +0700

    fix return api type

--- src/api/fetch/fetchActions.ts
+++ src/api/fetch/fetchActions.ts
@@ -1,9 +1,10 @@
 import aFetch from "../../axios";
 import {Action} from "../../core/classes";
+import {ActionDto} from "../../core/classes/dto";
 
 type ResponseType<T extends {}> = {
     ok:boolean
-    data?: Action<T>[]
+    data?: ActionDto[]
     message?: string
 }
 
@@ -12,16 +13,16 @@ const dateKeys = ['created_at', 'updated_at']
 export async function fetchActions<T extends {}>(time_ms:number){
     const result = (await aFetch.post<ResponseType<T>>('/actions/get/', {time: time_ms})).data
     if(result.ok && result.data){
-        const actions = result.data.map(a => new Action<T>(a))
-        actions.forEach(a => {
-            Object.keys(a.data)
-                .forEach(key => {
-                    if(key.includes('date') || dateKeys.includes(key)){
-                        // @ts-ignore
-                        a.data[key] = new Date(a.data[key])
-                    }
-                })
-        })
+        const actions = result.data
+        // actions.forEach(a => {
+        //     Object.keys(a.data)
+        //         .forEach(key => {
+        //             if(key.includes('date') || dateKeys.includes(key)){
+        //                 // @ts-ignore
+        //                 a.data[key] = new Date(a.data[key])
+        //             }
+        //         })
+        // })
         return actions
     } else {
         return []
