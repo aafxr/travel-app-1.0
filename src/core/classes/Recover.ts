59d85ce4d44b3d7bdeb3e05585baf4b758d06528
Alexandr707 <alexandralennikov90@gmail.com>
2024-05-16 16:02:33 +0700

    Recover store entities

--- src/core/classes/Recover.ts
+++ src/core/classes/Recover.ts
@@ -1,11 +1,10 @@
 import {DB} from "../db/DB";
 import {StoreName} from "../../types/StoreName";
-import {IndexName} from "../../types/IndexName";
 import {PredicateType} from "../../types/Predicate";
 import {Action, Expense, Hotel, Limit, Photo, Place, Travel} from "./store";
 import {ExpenseVariantType} from "../../types/ExpenseVariantType";
 import {ActionType} from "../../types/ActionType";
-import {PartialExpense, PartialHotel, PartialLimit, PartialPlace, PartialTravel} from "./store/partial";
+import {PartialExpense, PartialLimit} from "./store/partial";
 
 
 /**
@@ -14,107 +13,79 @@ import {PartialExpense, PartialHotel, PartialLimit, PartialPlace, PartialTravel}
 export class Recover {
     /**
      * метод востановления путешествия после пропадания интернета и наличия необработанных actions
-     * @param travelID ид путешествия
+     * @param id ид путешествия
      */
-    static async travel(travelID: string){
-        let t = new Travel({id: travelID})
-
-        const predicate = (a:Action<Partial<Travel>>) => a.entity === StoreName.TRAVEL && a.data.id === travelID
-
+    static async travel(id: string) {
+        let t = new Travel({id: id})
+        const predicate = (a: Action<PartialExpense>) => a.entity === StoreName.TRAVEL && a.data.id === id
         let actions = await DB.getLocalActions(predicate)
-
-        actions = actions.sort((a, b) => a.datetime.getTime() - b.datetime.getTime())
-
-        actions.forEach(a => {
-            const data = a.data
-            const keys = Object.keys(data)
-            for (const k of keys){
-                const key = k as keyof Travel
-
-                if(Array.isArray(data[key])){
-                    // @ts-ignore
-                    t[key] = Array.from(data[key])
-                } else if(data[key] && typeof data[key] === 'object'){
-                    // @ts-ignore
-                    if(!t[key]) t[key] = {}
-                    // @ts-ignore
-                    Object.assign(t[key], data[key])
-                } else{
-                    // @ts-ignore
-                    t[key] = data[key]
-                }
-            }
-        })
-
-        return t
+        return Recover._recordFieldsToTarget(actions, t)
     }
 
     static async expense(id: string, entityType: ExpenseVariantType) {
-        const result = new Expense({id, variant: entityType})
-        const predicate: PredicateType<Action<PartialExpense>> = (item) => item.entity === entityType && item.data.id === id
-        const cursor = await DB.openIndexCursor(StoreName.ACTION, IndexName.DATETIME, undefined, "next", predicate)
-        let expenseAction = (await cursor.next()).value
-        while (expenseAction) {
-            if (expenseAction.action === ActionType.DELETE) return
-            expenseAction = (await cursor.next()).value
-            if (expenseAction) Object.assign(result, expenseAction.data)
-        }
-        return result
+        const e = new Expense({id: id, variant: entityType})
+        const predicate = (a: Action<PartialExpense>) => a.entity === StoreName.EXPENSE && a.data.id === id
+        let actions = await DB.getLocalActions(predicate)
+        return Recover._recordFieldsToTarget(actions, e)
     }
 
     static async limit(id: string) {
-        const result = new Limit({id})
+        const l = new Limit({id})
         const predicate: PredicateType<Action<PartialLimit>> = (item) => item.entity === StoreName.LIMIT && item.data.id === id
-        const cursor = await DB.openIndexCursor(StoreName.ACTION, IndexName.DATETIME, undefined, "next", predicate)
-        let limitAction = (await cursor.next()).value
-        while (limitAction) {
-            if(limitAction.action === ActionType.DELETE) return
-            limitAction = (await cursor.next()).value
-            if (limitAction) Object.assign(result, limitAction.data)
-        }
-        return result
+        let actions = await DB.getLocalActions(predicate)
+        return Recover._recordFieldsToTarget(actions, l)
     }
 
 
-    static async place(id: string){
-        const result = new Place({id})
-        const predicate: PredicateType<Action<PartialPlace>> = (item) => item.entity === StoreName.PLACE && item.data.id === id
-        const cursor = await DB.openIndexCursor(StoreName.ACTION, IndexName.DATETIME, undefined, "next", predicate)
-        let placeAction = (await cursor.next()).value
-        while (placeAction) {
-            if(placeAction.action === ActionType.DELETE) return
-            placeAction = (await cursor.next()).value
-            if (placeAction) Object.assign(result, placeAction.data)
-        }
-        return result
+    static async place(id: string) {
+        const p = new Place({id})
+        const predicate: PredicateType<Action<PartialLimit>> = (item) => item.entity === StoreName.PLACE && item.data.id === id
+        let actions = await DB.getLocalActions(predicate)
+        return Recover._recordFieldsToTarget(actions, p)
     }
 
 
-    static async hotel(id: string){
-        const result = new Hotel({id})
-        const predicate: PredicateType<Action<PartialHotel>> = (item) => item.entity === StoreName.HOTELS && item.data.id === id
-        const cursor = await DB.openIndexCursor(StoreName.ACTION, IndexName.DATETIME, undefined, "next", predicate)
-        let hotelAction = (await cursor.next()).value
-        while (hotelAction) {
-            if(hotelAction.action === ActionType.DELETE) return
-            hotelAction = (await cursor.next()).value
-            if (hotelAction) Object.assign(result, hotelAction.data)
-        }
-        return result
+
+    static async hotel(id: string) {
+        const h = new Hotel({id})
+        const predicate: PredicateType<Action<PartialLimit>> = (item) => item.entity === StoreName.HOTELS && item.data.id === id
+        let actions = await DB.getLocalActions(predicate)
+        return Recover._recordFieldsToTarget(actions, h)
     }
 
 
-    static async photo(id: string){
-        const result = new Photo({id, base64: ''})
-        const predicate: PredicateType<Action<Photo>> = (item) => item.entity === StoreName.Photo && item.data.id === id
-        const cursor = await DB.openIndexCursor(StoreName.ACTION, IndexName.DATETIME, undefined, "next", predicate)
-        let photoAction = (await cursor.next()).value
-        while (photoAction) {
-            if(photoAction.action === ActionType.DELETE) return
-            photoAction = (await cursor.next()).value
-            if (photoAction) Object.assign(result, photoAction.data)
+    static async photo(id: string) {
+        const p = new Photo({id, base64: ''})
+        const predicate: PredicateType<Action<PartialLimit>> = (item) => item.entity === StoreName.Photo && item.data.id === id
+        let actions = await DB.getLocalActions(predicate)
+        return Recover._recordFieldsToTarget(actions, p)
+    }
+
+    static _recordFieldsToTarget<T extends Record<string, any>>(actions: Action<Partial<T>>[], target: T): T | undefined{
+        const _actions = actions.sort((a, b) => a.datetime.getTime() - b.datetime.getTime())
+        console.log(_actions)
+
+        for (const a of _actions) {
+            if(a.action === ActionType.DELETE) return
+            const data = a.data
+            const keys = Object.keys(data) as Array<keyof T>
+            for (const k of keys) {
+                const key = k as keyof Travel
+                if (Array.isArray(data[key])) {
+                    // @ts-ignore
+                    target[key] = Array.from(data[key])
+                } else if (data[key] && typeof data[key] === 'object') {
+                    // @ts-ignore
+                    if (!target[key]) target[key] = {}
+                    // @ts-ignore
+                    Object.assign(target[key], data[key])
+                } else {
+                    // @ts-ignore
+                    target[key] = data[key]
+                }
+            }
         }
-        return result
+        return target
     }
 
 }
